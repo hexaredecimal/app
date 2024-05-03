a1afab7372cc9f6b4015d06077d2b08a8fe9d43a
hexaredecimal <mfanakagama@gmail.com>
2024-05-03 09:20:59 +0200

    a pull up menu

--- www/js/card.js
+++ www/js/card.js
@@ -42,7 +42,7 @@ const createTestImage = (data) => {
 const showPopUp = (data) => {
   let name = data.getAttribute("data_name")
   let content = data.getAttribute("data_item").replaceAll("'", '"');
-  document.body.innerHTML += createPopUp(name, JSON.parse(content))
+  document.getElementById("card-container").innerHTML += createPopUp(name, JSON.parse(content))
 }
 
 const removeId = (id) => {
@@ -61,7 +61,7 @@ const createPopUp = (name, content) => {
     return ""
   }
 
-  console.log(content)
+  // console.log(content)
 
   let ingre = content.ingre.split(",");
   let price = content.price 
@@ -102,3 +102,21 @@ const createPopUp = (name, content) => {
 `
   return code 
 } 
+
+const createSlideDrawer = () => {
+  let element = 
+`
+    <div id="drawer-swipe" style="height: 60vh;" class="fixed z-40 w-full h-500 overflow-y-auto bg-white border-t border-gray-200 rounded-t-lg dark:border-gray-700 dark:bg-gray-800 transition-transform bottom-0 left-0 right-0 translate-y-full bottom-[0px]" tabindex="-1" aria-labelledby="drawer-swipe-label">
+       <div class="p-4 cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-700" data-drawer-toggle="drawer-swipe">
+          <span class="absolute w-8 h-1 -translate-x-1/2 bg-gray-300 rounded-lg top-3 left-1/2 dark:bg-gray-600"></span>
+          <h5 id="drawer-swipe-label" class="inline-flex items-center text-base text-gray-500 dark:text-gray-400 font-medium"><svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+          <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10ZM17 13h-2v-2a1 1 0 0 0-2 0v2h-2a1 1 0 0 0 0 2h2v2a1 1 0 0 0 2 0v-2h2a1 1 0 0 0 0-2Z"/>
+          </svg>Customize order</h5>
+        </div>
+    <div class="grid grid-cols-3 gap-4 p-4 lg:grid-cols-4"></div>
+    </div>
+`
+  console.log("ehe")
+  removeId("drawer-swipe")
+  return element
+}
